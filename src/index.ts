1f3e7d4d0eb83406486e44c22a8162240062ea85
gmishrax5 <gmishr56@gmail.com>
2026-07-27 08:41:31 +0530

    generating clients

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world");
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/mydb"
+});
+
+const client = new PrismaClient({ adapter });
+
+
+async function createUser(){
+await client.user.create({
+  data:{
+    username:"john",
+    password:"1876884",
+    age:20,
+    city:"indore"
+  }
+})
+
+}
+
+createUser();
